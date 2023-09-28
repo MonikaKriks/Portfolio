9d028ab6f9a9aed7b032493bc0a2afdc376e42d8
MonikaKriks <146135296+MonikaKriks@users.noreply.github.com>
2023-09-28 13:08:29 +0100

    Add files via upload
    week 1 task portfolio tasks completed

---
+++ Portfolio_wk1.py
@@ -0,0 +1,17 @@
+# task 4
+
+print('Temperatures that were reached over the summer:' )
+temp_c= 38.4
+temp_f =  1.8 * temp_c + 32
+print("Temperature in Celsius: ", temp_c )
+print("Temperature in Farenheit:" , temp_f)
+
+#############################################
+
+
+
+
+
+
+
+
