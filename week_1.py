76afffc823deac9619fff9844d7478a75dc256d5
MonikaK <146135296+MonikaKriks@users.noreply.github.com>
2023-10-04 12:02:18 +0100

    week_1.py
    added celsius and Fahrenheit symbols, rounded numbers up.

--- week_1.py
+++ week_1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 print('#task 1')
 if __name__ == '__main__':
-    print('Hello, World')
+    print('Hello, World\n')
 
 ###############################################
 
@@ -15,9 +15,11 @@ print( 'Hello, Monika\n')
 print('#task 3')
 print('Temperatures that were reached over the summer:' )
 temp_c= 38.4
+temp_c_round= round(38.4)
 temp_f =  1.8 * temp_c + 32
-print("Temperature in Celsius: ", temp_c )
-print("Temperature in Farenheit:" , temp_f)
+temp_f_round= round(1.8 * temp_c+32)
+print("Temperature in Celsius: ",temp_c_round,chr(176),'C')
+print("Temperature in Fahrenheit:" ,temp_f_round, chr(176),'F')
 
 #############################################
 
@@ -30,24 +32,21 @@ not_out = 162
 scored_runs = 48426
 
 batting_average = 1014/609
-print("Geoffrey Boycott's batting average is:",  batting_average , "hits per game.\n")
+batting_ave_rounded= round(1014/609,1)
+print("Geoffrey Boycott's batting average is:",  batting_ave_rounded , "hits per game.\n")
 
 #######################################################################################
 
+import math
+
 print ("# task 5")
-print('Student lab groups:' )
-students= 24, 113, 175, 12
-a_lab= 1
-b_lab= 5
-c_lab= 8
-d_lab = 0.5
-full_groups = a_lab + b_lab + c_lab
-left_over_group = 1
-print('Full student groups needed: ', full_groups)
-print('Left over group: ', left_over_group )
-
-#############################################################
-
+groups_needed= (113 + 175 + 12) /24
+print('Total student lab groups needed:', groups_needed,'\n')
+full_groups= int(groups_needed)
+left_over= math.ceil(0.5)
+print('Which means there will be: ')
+print(full_groups,'-', 'full groups')
+print(left_over,'-', 'left over group')
 
 
 
