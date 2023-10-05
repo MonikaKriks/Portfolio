b3409d99fbc6068334b2c7a6a3356e1871a4d8c4
MonikaK <146135296+MonikaKriks@users.noreply.github.com>
2023-10-05 11:40:30 +0100

    week_2.py

--- week_2.py
+++ week_2.py
@@ -1,1 +1,19 @@
+print("#task 1\n")
+name= input("Hello, what is your name? ")
+print("Hello", name,",","Good to meet you!" )
+
+##############################################
+
+print("#task 2\n")
+temp_c =input("Enter a temperature in Celsius: ")
+temp_f= float(5 * 1.8) + 32
+print(temp_c, "is equivalent to", temp_f, chr(176),"F")
+
+###############################################
+
+print("#task 3\n")
+total_students= input("How many students? ")
+students_per_group= input("Required group size? ")
+groups= total_students / students_per_group
+print(" There will be", groups, "groups with ... students left over")
 
